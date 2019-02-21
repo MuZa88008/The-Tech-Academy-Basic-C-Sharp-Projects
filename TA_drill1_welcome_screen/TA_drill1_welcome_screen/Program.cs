adb2b91a265e3fb1dd6ab5b847751f9010c032ac
Zach.Murray <muza.88008@gmail.com>
2019-02-21 06:00:49 -0700

    Tech Acadamey C# Drill - Accounting Welcome Screen
    Accounting Welcome Screen - TA Drill pg - 34

---
+++ TA_drill1_welcome_screen/TA_drill1_welcome_screen/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TA_drill1_welcome_screen
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to ZAMU Accounting Systems");
+            Console.WriteLine("Remember, We're \"accounting\" on you!");
+            Console.ReadLine();
+        }
+    }
+}
