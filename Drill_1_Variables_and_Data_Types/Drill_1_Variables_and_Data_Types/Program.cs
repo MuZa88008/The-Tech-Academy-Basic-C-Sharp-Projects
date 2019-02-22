b86bad677ccb0f2a7139aeaa1cd819fc2591f07c
Zach.Murray <muza.88008@gmail.com>
2019-02-22 06:36:49 -0700

    Drill programs committed
    Drills

---
+++ Drill_1_Variables_and_Data_Types/Drill_1_Variables_and_Data_Types/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Drill_1_Variables_and_Data_Types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Tech Academy");
+            Console.Write(Environment.NewLine);
+            Console.WriteLine("Student Daily Report");
+            Console.Write(Environment.NewLine);
+            Console.Write(Environment.NewLine);
+            Console.WriteLine("What course are you on?");
+            string crsvar = Console.ReadLine();
+            Console.WriteLine("What page?");
+            string pagevar = Console.ReadLine();
+            Console.WriteLine("Do you need help with anything? Please answer \"true\" or \"false\" ");
+            string help = Console.ReadLine();
+            Console.WriteLine("Were there any positive experiences you'd like to share? Please give specifics.");
+            string wins = Console.ReadLine();
+            Console.WriteLine("Is there any other feedback you’d like to provide? Please be specific.");
+            string feedback = Console.ReadLine();
+            Console.WriteLine("How many hours did you study today?");
+            string hours = Console.ReadLine();
+            Console.WriteLine("Thank you for your answers.  An Instructor will respond to this shortly.  Have a fantastic Day!");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Variables_and_Data_Types/Variables_and_Data_Types/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables_and_Data_Types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //
+            //Console.WriteLine("What is your name?");
+            //string yourName = Console.ReadLine();
+            //Console.WriteLine("Your name is: " + yourName);
+            //Console.ReadLine();
+
+            //Console.WriteLine("Pick a number...:");
+            //string Num1 = Console.ReadLine();
+            //int intNum1 = Convert.ToInt32(Num1);
+            //Console.WriteLine("Pick a second number...:");
+            //string Num2 = Console.ReadLine();
+            //int intNum2 = Convert.ToInt32(Num2);
+            //int total = intNum1 + intNum2;
+            //Console.WriteLine("Your total is...: " + total);
+            //Console.ReadLine();
+
+            //int currentAge = 42;
+            //string YearsOld = currentAge.ToString();
+
+            //Console.WriteLine("You are " + YearsOld + " years old.");
+            //Console.ReadLine();
+
+            bool amAwesome = true;
+            Console.WriteLine("Are you Awesome?");
+            Console.WriteLine("This is " + amAwesome);
+            Console.ReadLine();
+        }
+    }
+}
