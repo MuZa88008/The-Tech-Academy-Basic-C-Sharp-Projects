d5803f7f170bf47ad57ab35e29d552e1bb9865dd
Zach.Murray <muza.88008@gmail.com>
2019-02-23 15:39:13 -0700

    update data types
    update data types

--- Drill_1_Variables_and_Data_Types/Drill_1_Variables_and_Data_Types/Program.cs
+++ Drill_1_Variables_and_Data_Types/Drill_1_Variables_and_Data_Types/Program.cs
@@ -14,15 +14,15 @@ namespace Drill_1_Variables_and_Data_Types
             Console.WriteLine("What course are you on?");
             string crsvar = Console.ReadLine();
             Console.WriteLine("What page?");
-            string pagevar = Console.ReadLine();
+            int pagevar = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Do you need help with anything? Please answer \"true\" or \"false\" ");
-            string help = Console.ReadLine();
+            bool help = Convert.ToBoolean(Console.ReadLine());
             Console.WriteLine("Were there any positive experiences you'd like to share? Please give specifics.");
             string wins = Console.ReadLine();
             Console.WriteLine("Is there any other feedback you’d like to provide? Please be specific.");
             string feedback = Console.ReadLine();
             Console.WriteLine("How many hours did you study today?");
-            string hours = Console.ReadLine();
+            int hours = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Thank you for your answers.  An Instructor will respond to this shortly.  Have a fantastic Day!");
             Console.ReadLine();
 
