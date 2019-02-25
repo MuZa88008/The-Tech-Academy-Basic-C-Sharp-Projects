3284902fe533ecbe76ae8f963ef72e1b3a420323
Zach.Murray <muza.88008@gmail.com>
2019-02-25 05:17:42 -0700

    Do-While and While Drills
    Do-While and While Drills - TA Practical Drills p. 94

---
+++ Do_While_Drill/Do_While_Drill/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Do_While_Drill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello!");
+            Console.WriteLine("How many sides on a Nonagon?: ");
+            Console.WriteLine("** The answer is 9, but make a guess or several above and below 9 to see program logic...**");
+            int guess = Convert.ToInt32(Console.ReadLine());
+            bool test_expression = false;
+
+            do
+            {
+                if (guess == 9)
+                {
+                    Console.WriteLine("That is correct!");
+                    Console.ReadLine();
+                    test_expression = true;
+                }
+                else if (guess > 9)
+                {
+                    Console.WriteLine("Nice try... but guess lower...");
+                    guess = Convert.ToInt32(Console.ReadLine());
+                }
+                else if (guess < 9)
+                {
+                    Console.WriteLine("Good guess... try higher...");
+                    guess = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            while (test_expression == false);
+        }
+    }
+}
---
+++ While_Loop_Drill/While_Loop_Drill/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace While_Loop_Drill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number to count down from...:");
+            Console.WriteLine("**Program will countdown from number entered to zero (250ms) and beep at each count (+50hz each interval)...");
+            int count = Convert.ToInt32(Console.ReadLine());
+            int hertz = 500;
+
+            while (count > 0)
+            {
+                count--;
+                Thread.Sleep(250);
+                Console.Beep(hertz, 50);
+                Console.WriteLine(count);
+                hertz = hertz + 50;
+            }
+            Console.WriteLine("End... Press ENTER to exit");
+            Console.ReadLine();
+        }
+    }
+}
+
