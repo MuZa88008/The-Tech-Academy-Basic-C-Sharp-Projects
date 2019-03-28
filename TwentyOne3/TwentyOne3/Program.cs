256fbadaa50e53bd26b0bc92281f21f69ff650ed
Zach.Murray <muza.88008@gmail.com>
2019-03-28 07:25:39 -0600

    TwentyOne3 adding to web Github for access remotely

---
+++ TwentyOne3/TwentyOne3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOne3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CardClass cardOne = new CardClass();  //instantiate object - still empty however.
+            //cardOne.Face = "Queen";
+            //cardOne.Suit = "Hearts";
+
+            Console.WriteLine(cardOne.Face + " of " + cardOne.Suit);
+            Console.ReadLine();
+        }
+    }
+}
