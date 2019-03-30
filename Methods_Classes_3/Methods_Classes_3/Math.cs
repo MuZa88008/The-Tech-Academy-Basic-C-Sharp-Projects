baa1bb27b67fabbba913798c5ba5cb44034eafd9
Zach.Murray <muza.88008@gmail.com>
2019-03-30 17:56:05 -0600

    Methods Classes 3

---
+++ Methods_Classes_3/Methods_Classes_3/Math.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_Classes_3
+{
+    class Math
+    {
+        public int math(int num1)
+        {
+            return num1 * 100;
+        }
+
+        public decimal math(decimal num1)
+        {
+            return num1 * 100;
+        }
+    }
+}
---
+++ Methods_Classes_3/Methods_Classes_3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_Classes_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math math = new Math();
+            Console.WriteLine(math.math(12));
+            Console.ReadLine();
+
+            Console.WriteLine(math.math(10));
+            Console.ReadLine();
+        }
+    }
+}
