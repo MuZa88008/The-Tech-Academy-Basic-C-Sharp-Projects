b6b4459cd85b49ada0bd3ae881bbdbf4a2b8f779
Zach.Murray <muza.88008@gmail.com>
2019-03-31 08:43:06 -0600

    Methods Classes 4 addition
    C# & .Net Framework Cours - Drill page 115

---
+++ Methods_Classes_4/Methods_Classes_4/MathOperation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Methods_Classes_4
+{
+    class MathOperation
+    {
+        public int math (int num1, /*[System.Runtime.InteropServices.Optional]*/ int num2 = 0)
+        {
+            return (num1 + num2) * 1000;
+
+        }
+    }
+}
---
+++ Methods_Classes_4/Methods_Classes_4/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_Classes_4
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int num1;
+            int num2;
+
+            Console.WriteLine("Enter a whole number:    ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("You many enter another whole number if you want: ");
+            string entry = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(entry))
+            {
+                Console.WriteLine(num2 = 0);
+            }
+            else
+            {
+                num2 = Convert.ToInt32(entry);
+            }
+
+            MathOperation mathOperation = new MathOperation();
+
+            Console.WriteLine(mathOperation.math(num1, num2));
+            Console.ReadLine();
+
+        }
+    }
+}
