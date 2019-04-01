1716038439c3c5f6caf300c05b312ab1db9e9e96
MuZa88008 <45136605+MuZa88008@users.noreply.github.com>
2019-04-01 17:59:21 -0600

    Update to Methods Classes 5

--- Methods_Classes_5/Methods_Classes_5/Divide.cs
+++ Methods_Classes_5/Methods_Classes_5/Divide.cs
@@ -8,10 +8,18 @@ namespace Methods_Classes_5
 {
     class Divide
     {
-        public void Math(int num1)
+        public void Math(int num1, out int outResult)
         {
-            int result = num1 / 2;
-            Console.WriteLine(result);
+            outResult = num1 / 2;
+            //outresult = result;
+            //Console.WriteLine(result);
+        }
+
+        public void Math(decimal num1, out decimal outResult)
+        {
+            outResult = num1 / 2;
+            //outresult = result;
+            //Console.WriteLine(result);
         }
     }
 }
