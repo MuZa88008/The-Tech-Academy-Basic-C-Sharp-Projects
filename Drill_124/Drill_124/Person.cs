fed5d621d596ef354983b592e9922e0efebf15c5
MuZa88008 <45136605+MuZa88008@users.noreply.github.com>
2019-04-01 21:02:44 -0600

    Add Drill 124 - Updates to TwentyOne

---
+++ Drill_124/Drill_124/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill_124
+{
+    public abstract class Person
+    {
+        public virtual void SayName()
+        {
+            Console.WriteLine("Name: " + FirstName + " " + LastName);
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+
+    }
+}
